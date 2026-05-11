99bcfb2d579b47659c5f6540599853161d1d687a
belanika7 <belanika27@gmail.com>
2026-05-11 22:35:52 +0300

    test: add login automation project

---
+++ pages/LoginPage.ts
@@ -0,0 +1,19 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  usernameInput = '#user-name';
+  passwordInput = '#password';
+  loginButton = '#login-button';
+
+  async open() {
+    await this.page.goto('https://www.saucedemo.com/');
+  }
+
+  async login(username: string, password: string) {
+    await this.page.fill(this.usernameInput, username);
+    await this.page.fill(this.passwordInput, password);
+    await this.page.click(this.loginButton);
+  }
+}
